33f037f5d74f6eead5c5e4e878c8fc9f76d0c69f
Aleandr <saha.post@gmail.com>
2018-08-29 01:34:08 +0300

    credent 2

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -3,8 +3,9 @@
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
-  config.omniauth :twitter, ENV["TWITTER_APP_ID"], ENV["TWITTER_APP_SECRET"]
-  config.omniauth :facebook, ENV["FACEBOOK_APP_ID"], ENV["FACEBOOK_APP_SECRET"]
+  cred = Appl::Application.credentials
+  config.omniauth :twitter, cred.twitter[:api_id], cred.twitter[:api_secret]
+  config.omniauth :facebook, cred.facebook[:app_id], cred.facebook[:app_secret]
   
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
